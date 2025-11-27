5e3711a028d4af2b5b40a57cfaa9934e4df85073
Sergey Banny <sebanny@mail.ru>
2025-11-27 17:56:47 +0300

    Change structure of git branchs

--- backend/db/db_exceptions.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.exc import SQLAlchemyError
-
-
-class DBException(SQLAlchemyError):
-    """Base class for handling any SQLAlchemy-related errors."""
-
-    pass
--- backend/db/repositories/postgres/utils.py
+++
@@ -1,5 +0,0 @@
-import re
-
-
-def escape_tsquery(word: str) -> str:
-    return re.sub(r"[^a-zA-Zа-яА-Я0-9_]", "", word)
